e2c6988ff741384a26c169d9af9c18aa7a9118f1
Crystal Wright <wrightca5@appstate.edu>
2023-05-24 16:40:30 -0400

    hello

--- ascii_art.py
+++ ascii_art.py
@@ -1,14 +1,21 @@
 class ascii_art:
 
     def __init__(self):
-        self.desc = "";
+        self.desc = ""
 
     def draw_square():
+        print ("---------")
+        print("|         |")
+        print("|         |")
+        print("|         |")
+        print("|         |")
+        print ("---------")
         pass
 
     def draw_triangle():
+       
         pass
-        
+
     def draw_circle():
         pass
     
