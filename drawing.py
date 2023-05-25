32c3d476b26fac714b07f7b1897a62fe33fd25c8
Tayo Olofintuyi <tolofi152@gmail.com>
2023-05-25 11:30:11 -0400

    circle and triangle and move

--- drawing.py
+++ drawing.py
@@ -1,4 +1,5 @@
 import turtle
+import time
 
 class drawing:
     def __init__(self):
@@ -6,21 +7,39 @@ class drawing:
         pass
 
     def triangle(self):
+        self.mammal.forward(50)
+        self.mammal.right(120)
+        self.mammal.forward(50)
+        self.mammal.right(120)
+        self.mammal.forward(50)
+        self.mammal.right(120)
         pass
 
     def square(self):
         pass
 
     def circle(self):
-        pass
+        for i in range (50):
+            self.mammal.right(10)
+            self.mammal.forward(10)
+        
 
     def diamond(self):
         pass
+
+    def move(self, x: int = 0, y: int = 0):
+        self.mammal.penup()
+        self.mammal.goto(x,y)
+        self.mammal.pendown()
 
 
 if __name__ == '__main__':
     d = drawing()
     d.triangle()
+    d.move(50,50)
     d.circle()
     d.diamond()
     d.square()
+    turtle.done()
+    time.sleep(10)
+    
