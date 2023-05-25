35507eaf2a85af7273e35baa7c0c654a1ea7bfc0
Tayo Olofintuyi <tolofi152@gmail.com>
2023-05-25 12:32:06 -0400

    working with image manip

--- imageman.py
+++ imageman.py
@@ -1,4 +1,19 @@
+from PIL import Image, ImageEnhance
+
 class imageman:
 
     def __init__(self) -> None:
         pass
+
+if __name__ == '__main__':
+    #im = imageman()
+    img = Image.open(".\images\download.jpg")
+    newsize = (300, 300) 
+    img = img.resize(newsize) 
+    enhancer = ImageEnhance.Brightness(img) 
+    img_light = enhancer.enhance(1.8) 
+    img_light.save("images/doesthiswork.jpg")
+
+
+
+
