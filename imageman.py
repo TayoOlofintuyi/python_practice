0bd98de440049591889d6fb5c2b6bd2859165929
Tayo Olofintuyi <tolofi152@gmail.com>
2023-05-25 15:46:57 -0400

    image manip practice

--- imageman.py
+++ imageman.py
@@ -8,12 +8,23 @@ class imageman:
 
 if __name__ == '__main__':
     #im = imageman()
-    img = Image.open(".\images\download.jpg")
-    newsize = (300, 300) 
+    img = Image.open(".\images\HD-wallpaper-nature-beautiful.jpg")
+    newsize = (500, 1000) 
     img = img.resize(newsize) 
     enhancer = ImageEnhance.Brightness(img) 
-    img_light = enhancer.enhance(1.8) 
-    img_light.save("images/hmm.jpg")
+    img_light = enhancer.enhance(1.7) 
+    img_light.save("images/a.jpg")
+
+    changesize = (300, 600)
+
+    img1 = Image.open(".\images\download.jpg").resize(changesize)
+    img2 = Image.open(".\images\download2.jpg").resize(changesize)
+
+    img = Image.blend(im1 = img1, im2 = img2, alpha = 0.5)
+    enhancer = ImageEnhance.Brightness(img)
+    img = enhancer.enhance(1.5)
+
+    img.save("images/combo.png")
 
 
 
